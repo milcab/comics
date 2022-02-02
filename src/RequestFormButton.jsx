e905034a49fc56be0cb68b6dd420ae9dc2c1e4fa
milcab <milcarodriguez23@gmail.com>
2022-02-02 13:30:58 -0500

    creating the app

---
+++ src/RequestFormButton.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+
+export default function RequestFormButton() {
+  return (
+    <div>
+      <button>
+        Click Here!
+      </button>
+      <input type="submit" value="Submit" />
+
+    </div>
+  );
+}
