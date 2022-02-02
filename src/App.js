4e1d464ca17be27ff647aa38e76e0e95c90ca4d4
milcab <milcarodriguez23@gmail.com>
2022-02-02 14:13:33 -0500

    context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-// import React from 'react';
-import React from 'react';
-import './App.css';
-
-import UserInterface from "./UserInterface"
+import React from "react";
+import "./App.css";
+import UserInterface from "./UserInterface";
 import StylesContext from "./StylesContext";
-
 
 function App() {
   const stylin = {
@@ -15,27 +12,25 @@ function App() {
     fontSize: 18,
     letterSpacing: 1,
     border: "2px solid #1ECD97",
-    borderRadius: 15
+    borderRadius: 15,
   };
-
 
   return (
     <main>
-
       <StylesContext.Provider value={stylin}>
-      <h1
-        style={{
-          color: "#FEE001",
-          fontFamily: "Luminari, fantasy",
-          fontSize: 50
-        }}
-      >
-        Welcome to Comics Galore!
-      </h1>
-      <UserInterface />
+        <h1
+          style={{
+            color: "#FEE001",
+            fontFamily: "Luminari, fantasy",
+            fontSize: 50,
+          }}
+        >
+          Welcome to Comics Galore!
+        </h1>
+        <UserInterface />
       </StylesContext.Provider>
     </main>
   );
-} 
+}
 
-export default App;            
+export default App;
